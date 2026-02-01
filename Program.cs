18a2078d57638b1da9a577743e399cdab9c811a6
bartrumjustin <bartrumjustin@gmail.com>
2026-02-01 14:16:32 -0500

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,10 @@
+﻿namespace PrivateMultipleObjects
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+        }
+    }
+}
